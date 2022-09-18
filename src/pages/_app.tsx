40f397d814f6ae16a1fb869bcb25acc857c74a60
Caio Sousa <caiodelimack@hotmail.com>
2022-09-18 17:51:07 -0300

    refactor: ajustando app e home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,24 +2,16 @@ import React from "react";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 
-import { GlobalStyle, Main, Header } from "library-caiol.sousa";
-import { routes } from "../modules";
+import { Root } from "../layouts";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Header
-        routes={routes}
-        link={{ type: "next", variant: "doubleLine" }}
-        title="My-Fitness-CalculatorCLS"
-      />
-      <Main>
+      <Root>
         <Component {...pageProps} />
-      </Main>
-
-      <GlobalStyle />
+      </Root>
     </QueryClientProvider>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { Container } from "library-caiol.sousa";
 
-import { Container, Group } from "library-caiol.sousa";
+import { useStoreUser } from "../modules";
 
-const Base: React.FC = () => (
-  <Container gap={[30, 30]}>
-    <h1>Seja bem vindo ao MyFitnessCalculatorCLS!</h1>
-    <h2>PROJETO EM DESENVOLVIMENTO</h2>
-  </Container>
-);
+import { Home } from "../layouts";
+
+const Base: React.FC = () => {
+  const { resetUser } = useStoreUser();
+
+  return (
+    <Container gap={[30, 30]} direction="column">
+      <h1>Seja bem vindo ao MyFitnessCalculatorCLS!</h1>
+      <h2>PROJETO EM DESENVOLVIMENTO</h2>
+
+      <Home />
+    </Container>
+  );
+};
 
 export default Base;
